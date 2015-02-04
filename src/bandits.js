7deba551b7cf903af10932cc87b2d8896623470e
Venelin Valkov <venelin@bapplz.com>
2015-02-04 17:09:49 +0200

    Refactor arm/reward API, add total regret metric

--- src/bandits.js
+++ src/bandits.js
@@ -3,80 +3,78 @@ var rbeta = require("./stats").rbeta
 var pdfbeta = require("./stats").pdfbeta
 var beta = require("./stats").beta
 
-var Predictor = function(choices) {
+var Predictor = function(arms) {
 
     var self = {}
-    var bayesianBanditStrategy = BayesianBanditStrategy(choices)
+    var bayesianBanditStrategy = BayesianBanditStrategy(arms)
 
     self.predict = function() {
-        var arm = bayesianBanditStrategy.selectArm()
-        var choice = arm.choice
-        return {
-            id: arm.id,
-            choice: choice
-        }
+        var arm = bayesianBanditStrategy.maxPrizeArm()
+        return arm
     }
 
     self.learn = function(prediction, result) {
         bayesianBanditStrategy.update(prediction, result)
     }
 
-    self.probabilities = function() {
-        return bayesianBanditStrategy.armProbabilities()
+    self.posteriorProbabilities = function() {
+        return bayesianBanditStrategy.armPosteriors()
     }
 
     return self
 }
 
-var Arm = function(id, choice) {
+function createArm(id, reward) {
+    return Arm(id, reward, 0 ,0)
+}
+
+var Arm = function(id, reward, wins, trials) {
 
     var self = {}
 
-    self.wins = 0
-    self.trials = 0
-    self.choice = choice
     self.id = id
+    self.reward = reward
+    self.wins = wins
+    self.trials = trials
 
     self.losses = function() {
         return self.trials - self.wins
     }
 
-    self.probability = function() {
+    self.prizeProbability = function() {
         return rbeta(1 + self.wins, 1 + self.losses())
+    }
+
+    self.posterior = function(range) {
+        return pdfbeta(range, 1 + self.wins, 1 + self.losses()) 
     }
 
     return self
 }
 
-var BayesianBanditStrategy = function(choices) {
+var BayesianBanditStrategy = function(arms) {
 
     var self = {}
-    var armId = 0
-    self.arms = choices.map(function(x) { 
-        return Arm(armId++, x) 
-    })
+    self.arms = arms
 
-    self.selectArm = function() {
-        return _.max(self.arms, function(arm) {
-            return arm.probability()
-        })
+    self.maxPrizeArm = function() {
+        return _.max(self.arms, function(a) { return a.prizeProbability() })
     }
 
-    self.update = function(choice, result) {
-        var indexedArms = _.indexBy(self.arms, 'id')
-        var arm = indexedArms[choice.id]
+    self.update = function(arm, result) {
+        var arm = _.find(self.arms, function(a) { return a.id == arm.id })
         arm.wins += result
         arm.trials += 1
     }
 
-    self.armProbabilities = function() {
+    self.armPosteriors = function() {
         x = _.range(0, 1, 0.01)
-        return self.arms.map(function(arm) {
-            return pdfbeta(x, 1 + arm.wins, 1 + arm.losses())
-        })
+        return self.arms.map(function(a) { return a.posterior(x) })
     }
 
     return self
 }
 
 module.exports.Predictor = Predictor
+module.exports.Arm = Arm
+module.exports.createArm = createArm
--- test/bandit_test.js
+++ test/bandit_test.js
@@ -1,6 +1,8 @@
 var should = require('chai').should()
 var _ = require("underscore")
 var Predictor = require('../src/bandits').Predictor
+var Arm = require('../src/bandits').Arm
+var createArm = require('../src/bandits').createArm
 
 describe("Bandit Predictor", function() {
 
@@ -9,24 +11,29 @@ describe("Bandit Predictor", function() {
     }
 
     beforeEach(function* () {
-        global.data = {}
-        global.choices = [1, 2]
-        global.predictor = Predictor(choices)
+        global.rewards = [1, 2]
+        global.arm1id = 1
+        global.arm2id = 2
+        global.predictor = Predictor([
+            createArm(arm1id, rewards[0]),
+            createArm(arm2id, rewards[1])])
     })
 
-    it("should give choice", function* () {
-        var prediction = predictor.predict(data) 
-        //prediction.should.be.within(choices[0], choices[1])
+    it("should give reward", function* () {
+        var arm = predictor.predict() 
+        arm.reward.should.be.within(rewards[0], rewards[1])
     })
 
     it("should learn hidden probability", function* () {
-        var idsToProbability = [0.4, 0.8]
+        var armProbabilities = {}
+        armProbabilities[arm1id] = 0.4
+        armProbabilities[arm2id] = 0.8
         for (var i = 0; i < 1000; i++) {
-            var prediction = predictor.predict() 
-            var p = idsToProbability[prediction.id]
-            predictor.learn(prediction, predictionResult(p))
+            var arm = predictor.predict() 
+            var p = armProbabilities[arm.id]
+            predictor.learn(arm, predictionResult(p))
         }
-        test2Prob = predictor.probabilities()[1]
+        test2Prob = predictor.posteriorProbabilities()[1]
         mode = test2Prob.indexOf(_.max(test2Prob))
         mode.should.be.within(75, 85)
     })
